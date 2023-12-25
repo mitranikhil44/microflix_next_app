61b409d6ffef9665752e1822499cb1cbd42b940f
mitranikhil44 <mitranikhil93@gmail.com>
2023-12-25 22:16:22 +0530

    Initial commit

---
+++ app/sitemap/page.xml.js
@@ -0,0 +1,44 @@
+
+import axios from 'axios';
+
+const generateSitemapXml = (slugs) => {
+  const baseUrl = 'https://microflix.vercel.app/'; 
+  const pages = slugs.map((slug) => ({
+    loc: `${baseUrl}/${slug}`,
+    lastmod: new Date().toISOString(),
+    changefreq: 'daily',
+    priority: 0.7,
+  }));
+
+  const xml = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${pages
+        .map(
+          (page) => `<url>
+        <loc>${page.loc}</loc>
+        <lastmod>${page.lastmod}</lastmod>
+        <changefreq>${page.changefreq}</changefreq>
+        <priority>${page.priority}</priority>
+      </url>`
+        )
+        .join('')}
+    </urlset>
+  `;
+
+  return xml.trim();
+};
+
+export default async function Sitemap() {
+    const apiKey = process.env.API_KEY || "https://microflix.vercel.app/";
+  try {
+    const response = await axios.get(`${apiKey}api/getAllSlugs`, {"cache": "no-store"}); 
+    const slugs = response.data.query.map((item) => item.slug);
+    const sitemapXml = generateSitemapXml(slugs);
+
+    return <>{sitemapXml}</>;
+  } catch (error) {
+    console.error('Error fetching slugs:', error);
+    return null;
+  }
+}
