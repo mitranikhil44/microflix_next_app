b8bd20dc0e53ead69ea3b3add7335d8240fdf557
mitranikhil44 <mitranikhil93@gmail.com>
2023-12-27 16:40:39 +0530

    Initial commi

--- app/api/sitemap.xml/route.js
+++ app/api/sitemap.xml/route.js
@@ -1,35 +1,34 @@
 import connectToDatabase from '@/lib/mongodb';
 import { Contents } from '@/models/scrapeSchema';
-import { NextResponse } from 'next/server';
 
 export async function GET(req) {
-    await connectToDatabase();
-    try {
-        const query = await Contents.find({}, { slug: 1 }).exec();
+  await connectToDatabase();
 
-        const slugs = query.map((item) => item.slug);
-        const sitemapXml = generateSitemapXml(slugs);
+  try {
+    const query = await Contents.find({}, { slug: 1 }).exec();
+    const slugs = query.map((item) => item.slug);
+    const sitemapXml = generateSitemapXml(slugs);
 
-        return new Response(sitemapXml, {
-          status: 200,
-          headers: { 'Content-Type': 'application/xml' },
-        })
-    } catch (error) {
-        console.error('Error fetching slugs:', error);
-        return NextResponse.text('Internal Server Error', { status: 500 });
-    }
+    return new Response(sitemapXml, {
+      status: 200,
+      headers: { 'Content-Type': 'application/xml' },
+    });
+  } catch (error) {
+    console.error('Error fetching slugs:', error);
+    return new Response('Internal Server Error', { status: 500 });
+  }
 }
 
 const generateSitemapXml = (slugs) => {
-    const baseUrl = 'https://microflix.vercel.app/';
-    const pages = slugs.map((slug) => ({
-        loc: `${baseUrl}/${slug}`,
-        lastmod: new Date().toISOString(),
-        changefreq: 'daily',
-        priority: 0.7,
-    }));
+  const baseUrl = 'https://microflix.vercel.app/';
+  const pages = slugs.map((slug) => ({
+    loc: `${baseUrl}/${slug}`,
+    lastmod: new Date().toISOString(),
+    changefreq: 'daily',
+    priority: 0.7,
+  }));
 
-    const xml = `
+  const xml = `
     <?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${pages
@@ -45,5 +44,5 @@ const generateSitemapXml = (slugs) => {
     </urlset>
   `;
 
-    return xml.trim();
+  return xml.trim();
 };
--- app/page.js
+++ app/page.js
@@ -1,62 +1,27 @@
 import React from "react";
 import MoviesCollection from "../components/Movie_Collection";
 
-const categoriesConfig = [
-  { category: "contents", label: "Contents" },
-  { category: "top_contents", label: "Top Contents" },
-  { category: "content_movies", label: "Movies" },
-  { category: "top_content_movies", label: "Top Movies" },
-  { category: "content_seasons", label: "Web Series" },
-  { category: "top_content_seasons", label: "Top Web Series" },
-  { category: "content_adult", label: "18+ Contents" },
-  { category: "top_content_adult", label: "Top 18+ Contents" },
-];
-
 export default async function Home() {
-  const response = await backendData();
-  const data = response.props.data;
+  const data = await fetchContents("contents");
+  const contents = data[0].data
   return (
     <main>
       <div className="text-lg">This page is under construction please explore other pages</div>
-      {/* {categoriesConfig.map((config, index) => (
+      {/* {contents && contents.map((elem, index) => (
         <MoviesCollection
-          key={index + 1}
-          data={data[config.category][0].data}
-          collectionName={config.label}
-          linkPath={`/data/${config.category}`}
+        key={index + 1}
+        data={elem}
+        collectionName="Contents"
+        linkPath={`/data/contents`}
         />
-      ))} */}
-
+        ))} */}
     </main>
   );
 }
 
-async function fetchData(category, apiKey) {
-  const response = await fetch(`${apiKey}/api/blogs/?category=${category}&page=1`, {
-    headers: {
-      'Cache-Control': 'no-store',
-    },
-  });
-
-  if (response.ok) {
-    const data = await response.json();
-    return { [category]: data };
-  } else {
-    console.error(`Error while fetching data for ${category}: ${response.statusText}`);
-    return { [category]: { error: 'Error while fetching data' } };
-  }
-}
-
-export async function backendData() {
-  const apiKey = process.env.API_KEY || "https://microflix.vercel.app/";
-  const promises = categoriesConfig.map((config) => fetchData(config.category, apiKey));
-
-  try {
-    const results = await Promise.all(promises);
-    const data = results.reduce((acc, result) => ({ ...acc, ...result }), {});
-    return { props: { data } };
-  } catch (error) {
-    console.error("Error:", error);
-    return { props: { data: {} } };
-  }
+const fetchContents = async (category) =>{
+  const apiKey = process.env.API_KEY
+  const response = await fetch(`${apiKey}api/blogs/?category=${category}&page=1`, {cache: "no-store"});
+  const data = await response.json();
+  return data
 }
